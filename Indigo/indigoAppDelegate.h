46206ee600a63d01bc5e3d9f5a8637b15b1a6c11
hoangnam-aainc <hoang.nam@aainc.co.jp>
2014-06-26 15:17:59 +0900

    Initial Commit

---
+++ Indigo/indigoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  indigoAppDelegate.h
+//  Indigo
+//
+//  Created by Hoang Nam on 2014/06/26.
+//  Copyright (c) 2014年 Hoang Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface indigoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Indigo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Indigo
+//
+//  Created by Hoang Nam on 2014/06/26.
+//  Copyright (c) 2014年 Hoang Nam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "indigoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([indigoAppDelegate class]));
+    }
+}
